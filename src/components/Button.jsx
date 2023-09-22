ab129689412caaa55344773cfa0678a8e11453cf
cyblogerz <pranavajay2002@gmail.com>
2023-09-22 19:55:37 +0530

    Updated buttons'

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,20 @@
 
 const Button = (props) => {
   return (
-    <button class="px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm text-xl mt-16">Kanify me 🎤</button>
+    <button class="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text text-2xl 
+    hover:from-purple-500 hover:to-blue-500
+    transition duration-300 ease-in-out transform hover:scale-105
+    md:text-3xl font-bold mt-8">
+  Kanify me 
+</button>
+
+
   )
 }
 
 export default Button;
+
+{/* <button class="
+bg-gradient-to-r from-blue-500 to-purple-500 text-white hover:from-purple-500 hover:to-blue-500 hover:text-white py-2 px-4 rounded-sm  transition duration-300 ease-in-out transform hover:scale-105
+px-4 py-2   text-white rounded-full shadow-lg text-xl mt-16">Kanify me 🎤</button>
+) */}
